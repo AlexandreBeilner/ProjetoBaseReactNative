1bdbd4829eaac8caa8db28d616a40eed1c1ecf47
AlexandreBeilner <alexandrebeilner8@gmail.com>
2024-03-25 15:12:32 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+const App: React.FC = () => {
+  return(
+    <Text style={{fontFamily: "Nunito-Regular"}}>App</Text>
+  )
+}
+
+export {App}
